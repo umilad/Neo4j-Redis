d3fb1f3ad3e04934f3feadd2b1448e3b9bba0e0f
unknown <anita.aleksic@elfak.rs>
2025-02-05 16:39:07 +0100

    neo4j aura

--- Back/Neo4jService.cs
+++ Back/Neo4jService.cs
@@ -13,4 +13,48 @@ public class Neo4jService
     {
         return _driver.AsyncSession();
     }
+
+    //Clean up resources
+    // public void Dispose()
+    // {
+    //     _driver?.Dispose();
+    // }
 }
+// using System;
+// using System.Threading.Tasks;
+// using Neo4j.Driver;
+
+// public class Neo4jService : IDisposable
+// {
+//     private readonly IDriver _driver;
+
+//     // Constructor to initialize the connection with the database
+//     public Neo4jService(string uri, string user, string password)
+//     {
+//         _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+//     }
+
+//     // Method to run a simple query (e.g., fetching nodes)
+//     public async Task ReadDataAsync()
+//     {
+//         var session = _driver.AsyncSession();
+//         try
+//         {
+//             var result = await session.RunAsync("MATCH (n) RETURN n LIMIT 5");
+//             while (await result.FetchAsync())
+//             {
+//                 Console.WriteLine(result.Current["n"]);
+//             }
+//         }
+//         finally
+//         {
+//             await session.CloseAsync();
+//         }
+//     }
+
+//     // Clean up resources
+//     public void Dispose()
+//     {
+//         _driver?.Dispose();
+//     }
+// }
--- Back/Program.cs
+++ Back/Program.cs
@@ -1,8 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddSingleton<Neo4jService>(new Neo4jService("neo4j://localhost:7999", "neo4j", "password"));
-builder.Services.AddControllers();
+builder.Services.AddSingleton<Neo4jService>(new Neo4jService("neo4j+s://84307b33.databases.neo4j.io",
+ "neo4j",
+ "AEYgjhymGHlfJt2zx5MRhtkd6HZaSWCIQGgMITq0u6E"));
+
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
